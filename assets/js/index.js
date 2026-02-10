12560ee7782d46c54dcb21cc1652607310c5736b
unknown <devliyalhimanshu@gmsil.com>
2026-02-10 18:17:36 +0530

    logo changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,6 +8,38 @@ window.addEventListener("scroll", function () {
   }
 });
 
+document.addEventListener("click", function (event) {
+  const navbar = document.getElementById("navbarNav");
+  const toggler = document.querySelector(".navbar-toggler");
+
+  // agar navbar open hai
+  if (navbar.classList.contains("show")) {
+    // click navbar ke andar nahi hua + toggler par bhi nahi
+    if (!navbar.contains(event.target) && !toggler.contains(event.target)) {
+      toggler.click(); // navbar close
+    }
+  }
+});
+
+window.addEventListener("scroll", function () {
+  const floatingBtn = document.querySelector(".floating-btn");
+
+  const scrollTop = window.scrollY;
+  const windowHeight = window.innerHeight;
+  const docHeight = document.documentElement.scrollHeight;
+
+  // kitna page bacha hai niche
+  const remainingScroll = docHeight - (scrollTop + windowHeight);
+
+  if (remainingScroll <= 100) {
+    // footer ke paas → hide
+    floatingBtn.classList.add("hide");
+  } else {
+    // footer se door → show
+    floatingBtn.classList.remove("hide");
+  }
+});
+
 document.getElementById("quoteForm").addEventListener("submit", function (e) {
   e.preventDefault();
 
